6bb9ce3dfbb7fe85267082fe15078c1f89055ed0
Brent Gardner <bgardner@squarelabs.net>
2019-01-05 11:47:28 -0700

    Read parquet file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+extern crate parquet;
+
+use std::env;
+use std::fs::File;
+use std::path::Path;
+use parquet::record::RowAccessor;
+use parquet::file::reader::{FileReader, SerializedFileReader};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<_> = env::args().collect();
+    let path = &args[1];
+    println!("Reading path: {}", path);
+    let file = File::open(&Path::new(&path)).unwrap();
+    let reader = SerializedFileReader::new(file).unwrap();
+    let mut iter = reader.get_row_iter(None).unwrap();
+    while let Some(record) = iter.next() {
+        record.get_bool(0);
+        println!("{}", record);
+    }
 }
