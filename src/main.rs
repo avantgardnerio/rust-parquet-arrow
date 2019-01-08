a82bfd2a8ef4084ffd0ac11dd6ec94d9f9d28af3
Brent Gardner <bgardner@squarelabs.net>
2019-01-07 17:54:15 -0700

    Working on GroupTypes

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,7 @@ use std::any::Any;
 use std::env;
 use std::fs::File;
 use std::path::Path;
+use parquet::basic::Repetition;
 
 fn main() {
     let args: Vec<_> = env::args().collect();
@@ -40,12 +41,12 @@ fn main() {
                     let val = record.get_double(i).unwrap();
                     let column: &mut Vec<f64> = data[i].downcast_mut().unwrap();
                     column.push(val);
-                },
+                }
                 DataType::Utf8 => {
                     let val = record.get_string(i).unwrap().clone();
                     let column: &mut Vec<String> = data[i].downcast_mut().unwrap();
                     column.push(val);
-                },
+                }
                 _ => panic!("Unknown type: {:?}", fields[i].data_type())
             }
         }
@@ -64,105 +65,90 @@ fn main() {
 fn type2vec(t: &parquet::schema::types::Type) -> Box<Any> {
     use parquet::basic::Type;
     match t {
-        PrimitiveType {
-            basic_info,
-            physical_type,
-            type_length: _,
-            scale: _,
-            precision: _
-        } => {
+        PrimitiveType { basic_info, physical_type, type_length: _, scale: _, precision: _ } => {
             match physical_type {
-                Type::BOOLEAN => {
-                    return Box::new(Vec::<bool>::new());
-                }
-                Type::INT32 => {
-                    return Box::new(Vec::<i32>::new());
-                },
-                Type::INT64 => {
-                    return Box::new(Vec::<i64>::new());
-                },
-                Type::INT96 => {
-                    panic!("INT96 not implemented!");
-                },
-                Type::FLOAT => {
-                    return Box::new(Vec::<f32>::new());
-                },
-                Type::DOUBLE => {
-                    return Box::new(Vec::<f64>::new());
-                },
+                Type::BOOLEAN => return Box::new(Vec::<bool>::new()),
+                Type::INT32 => return Box::new(Vec::<i32>::new()),
+                Type::INT64 => return Box::new(Vec::<i64>::new()),
+                Type::INT96 => panic!("INT96 not implemented!"),
+                Type::FLOAT => return Box::new(Vec::<f32>::new()),
+                Type::DOUBLE => return Box::new(Vec::<f64>::new()),
                 Type::BYTE_ARRAY => {
                     match basic_info.logical_type() {
-                        LogicalType::UTF8 => {
-                            return Box::new(Vec::<String>::new());
-                        }
-                        _ => {
-                            panic!("Unknown type!");
-                        }
+                        LogicalType::UTF8 => return Box::new(Vec::<String>::new()),
+                        _ => panic!("Unknown type!")
                     }
-                },
-                Type::FIXED_LEN_BYTE_ARRAY => {
-                    panic!("Unknown type!");
                 }
+                Type::FIXED_LEN_BYTE_ARRAY => panic!("Unknown type!")
             }
-        },
-        GroupType { basic_info: _, fields: _ } => {
-            panic!("Unknown type!");
         }
+        GroupType { basic_info: _, fields: _ } => panic!("Unknown type!")
     }
 }
 
 fn type2field(t: &parquet::schema::types::Type) -> Field {
     use parquet::basic::Type;
     match t {
-        PrimitiveType {
-            basic_info,
-            physical_type,
-            type_length: _,
-            scale: _,
-            precision: _
-        } => {
-            let nullable = if basic_info.has_repetition() {
-                basic_info.repetition() != parquet::basic::Repetition::REQUIRED
-            } else {
-                false
-            };
+        PrimitiveType { basic_info, physical_type, type_length: _, scale: _, precision: _ } => {
+            let repetition = if basic_info.has_repetition() { Some(basic_info.repetition()) } else { None };
+            let nullable = repetition.unwrap_or(Repetition::OPTIONAL) == Repetition::OPTIONAL;
             let name = basic_info.name();
             match physical_type {
-                Type::BOOLEAN => {
-                    return Field::new(name, DataType::Boolean, nullable);
-                },
-                Type::INT32 => {
-                    return Field::new(name, DataType::Int32, nullable);
-                },
-                Type::INT64 => {
-                    return Field::new(name, DataType::Int64, nullable);
-                },
-                Type::INT96 => {
-                    panic!("INT96 not implemented!");
-                },
-                Type::FLOAT => {
-                    return Field::new(name, DataType::Float32, nullable);
-                },
-                Type::DOUBLE => {
-                    return Field::new(name, DataType::Float64, nullable);
-                },
+                Type::BOOLEAN => return Field::new(name, DataType::Boolean, nullable),
+                Type::INT32 => return Field::new(name, DataType::Int32, nullable),
+                Type::INT64 => return Field::new(name, DataType::Int64, nullable),
+                Type::INT96 => panic!("INT96 not implemented!"),
+                Type::FLOAT => return Field::new(name, DataType::Float32, nullable),
+                Type::DOUBLE => return Field::new(name, DataType::Float64, nullable),
                 Type::BYTE_ARRAY => {
                     match basic_info.logical_type() {
-                        LogicalType::UTF8 => {
-                            return Field::new(name, DataType::Utf8, nullable);
-                        }
-                        _ => {
-                            panic!("Unknown type!");
-                        }
+                        LogicalType::UTF8 => return Field::new(name, DataType::Utf8, nullable),
+                        _ => panic!("Unknown type!")
                     }
-                },
-                parquet::basic::Type::FIXED_LEN_BYTE_ARRAY => {
+                }
+                Type::FIXED_LEN_BYTE_ARRAY => panic!("Unknown type!")
+            }
+        }
+        GroupType { basic_info, fields } => {
+            match basic_info.logical_type() {
+                LogicalType::LIST => {
+//                    let field = Field::new()
+//                    let data_type = DataType::Struct(field);
+//                    return Field::new(name, data_type, nullable);
+                    let len = fields.len();
+                    fields.iter().for_each( |f| {
+                        println!("{:?} {:?}", len, f);
+                        panic!("Unknown type!");
+                    });
                     panic!("Unknown type!");
                 }
+                _ => panic!("Unknown type!")
             }
-        },
-        GroupType { basic_info: _, fields: _ } => {
-            panic!("Unknown type!");
         }
     };
 }
+
+/*
+GroupType {
+    basic_info: BasicTypeInfo {
+        name: "list",
+        repetition: Some(REPEATED),
+        logical_type: NONE,
+        id: None
+    },
+    fields: [
+        PrimitiveType {
+            basic_info: BasicTypeInfo {
+                name: "element",
+                repetition: Some(OPTIONAL),
+                logical_type: NONE,
+                id: None
+            },
+            physical_type: INT64,
+            type_length: -1,
+            scale: -1,
+            precision: -1
+        }
+    ]
+}
+*/
